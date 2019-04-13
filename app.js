800205b4a13029c72cac9e60735778b2da2982b5
IceHax <vecchiotom1@gmail.com>
2019-04-13 22:43:39 +0200

    add self pinger

--- app.js
+++ app.js
@@ -2,17 +2,15 @@ var express = require('express');
 var app = express();
 var port = process.env.PORT || 80;
 var exphbs = require('express-handlebars')
+
+//start self pinger
+require('heroku-self-ping')("http://eggbay.herokuapp.com");
  
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
  
 app.get('/', function (req, res) {
     res.render('home');
-});
-
-//web crawler
-app.get('/wakemydyno.txt', function (req, res) {
-    res.send("hi bot")
 });
 
 app.listen(port);
